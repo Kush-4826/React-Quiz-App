2b569a0edd4d6c8f62961c306089bdbaa601a702
Kush Agarwal <agarwalkushajay@gmail.com>
2024-04-02 20:17:11 +0530

    Removed Obfuscation

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import obfuscator from 'rollup-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    obfuscator({
-      global: true
-    })
   ],
 })
